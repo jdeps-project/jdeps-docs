a116d04209ed526c673bfba7dcd950240e00153b
danielxiao <93750574@163.com>
2025-07-11 19:02:54 +0800

    添加 candle 的隐私政策

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'en-US',
+  base: '/jdeps-docs/', // 注意必须以 / 开头和结尾
+  title: 'Jdeps-Doc',
+  description: 'My first VuePress Site',
+
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: viteBundler(),
+})
